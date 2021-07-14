0795c58d6b7fc9f4400780eb07f9b2e02be3a0ee
Matthew Walker <matthew.r.walker4@gmail.com>
2021-07-13 23:13:48 -0500

    added more practice java

---
+++ src/ArrayStuff.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArrayStuff {
+
+    // find the missing number in the array of "1-50"
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+                31, 32, 33, 34, 35, 36, 37, 38, 39,
+                40, 41, 42, 43, 45, 46, 47, 48, 49, 50};
+
+        for(int i = 0; i <= nums.length; ++i) {
+            if (i + 1 != nums[i]) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+    }
+}
